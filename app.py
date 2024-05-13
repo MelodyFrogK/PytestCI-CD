6238fe133a4ab21d3437d5f0ca7e17a9bfb43bef
guru <guru@node2.example.com>
2024-05-13 18:11:27 +0900

    reset

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+
+from utils import get_movie_info
 
 app = Flask(__name__)
 
+from pymongo import MongoClient
+
+client = MongoClient('mongo', 27017)
+db = client.dbsparta
+
+
+## HTML을 주는 부분
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html')
 
-def some_function():
-    return "Hello, World!"
 
-class SomeClass:
-    def method(self):
-        return "Method called"
+@app.route('/memo', methods=['GET'])
+def listing():
+    articles = list(db.articles.find({}, {'_id': False}))
+    return jsonify({'all_articles': articles})
+
+
+## API 역할을 하는 부분
+@app.route('/memo', methods=['POST'])
+def saving():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+
+    title, image, desc = get_movie_info(url_receive)
+    doc = {
+        'title': title,
+        'image': image,
+        'desc': desc,
+        'url': url_receive,
+        'comment': comment_receive
+    }
+
+    db.articles.insert_one(doc)
+
+    return jsonify({'msg': '저장이 완료되었습니다!'})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run('0.0.0.0', port=5000, debug=True)
--- test_app.py
+++
@@ -1,9 +0,0 @@
-# test_app.py
-from app import some_function, SomeClass
-
-def test_some_function():
-    assert some_function() is None
-
-def test_SomeClass():
-    assert isinstance(SomeClass(), SomeClass)
-
